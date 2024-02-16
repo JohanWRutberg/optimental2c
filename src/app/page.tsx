6b9ad3bedcaec6f8aa81d44006f99f3725513722
nybergjack <nybergjack@gmail.com>
2024-02-16 08:28:57 +0100

    La till ett extra lager i parallaxen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "../app/components/Hero";
 import { sendMail } from "@/lib/mail";
 import Parallax from "./components/Parallax";
 
-export default async function Home() {
+export default function Home() {
   return (
     <main>
       <Parallax />
